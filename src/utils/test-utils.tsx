65ad2f90c20b75309586896b4f5deb6fed4d50d3
Vo Minh Thong <56250318+thongvmdev@users.noreply.github.com>
2024-12-17 20:20:52 +0700

    feat: add tests Product List (#10)
    Co-authored-by: Vo Minh Thong [James][FE] <thong.vo@navercorp.com>

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,11 +1,34 @@
 import { RenderOptions, render as rtlRender } from '@testing-library/react'
 import { ReactElement } from 'react'
+import { IProduct } from '~/interfaces'
 
 function render(ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) {
   return rtlRender(ui, {
     ...options
   })
 }
+
+export const mockProduct = {
+  id: 6,
+  title: 'Bassbreaker',
+  category: 'Lower Body',
+  price: 161.04,
+  isFavorite: true,
+  createdAt: 1715002382000,
+  theme: 'Dark',
+  tier: 'Premium',
+  imageId: 6,
+  author: {
+    firstName: 'Mead',
+    lastName: 'Niccols',
+    email: 'mniccols5@ameblo.jp',
+    gender: 'Male',
+    avatar: 'https://robohash.org/quoindolore.png?size=100x100&set=set1',
+    onlineStatus: 'idle'
+  },
+  backgroundColor: 'linear-gradient(90deg, #43a6f6 50%, #5868f3 100%)',
+  ethPath: '/images/eth-avatar/image-three.png'
+} as IProduct
 
 export * from '@testing-library/react'
 export { render }
