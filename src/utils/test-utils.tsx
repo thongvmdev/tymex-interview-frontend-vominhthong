2249baf563897b286569a91e07892d7cd5b66187
Vo Minh Thong [James][FE] <thong.vo@navercorp.com>
2024-12-17 23:42:42 +0700

    feat: add tests marketplace hooks

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -30,5 +30,27 @@ export const mockProduct = {
   ethPath: '/images/eth-avatar/image-three.png'
 } as IProduct
 
+export const mockProduct2 = {
+  id: 7,
+  title: 'Bassbreaker2',
+  category: 'Lower Body',
+  price: 161.04,
+  isFavorite: true,
+  createdAt: 1715002382000,
+  theme: 'Dark',
+  tier: 'Premium',
+  imageId: 6,
+  author: {
+    firstName: 'Mead',
+    lastName: 'Niccols',
+    email: 'mniccols5@ameblo.jp',
+    gender: 'Male',
+    avatar: 'https://robohash.org/quoindolore.png?size=100x100&set=set1',
+    onlineStatus: 'idle'
+  },
+  backgroundColor: 'linear-gradient(90deg, #43a6f6 50%, #5868f3 100%)',
+  ethPath: '/images/eth-avatar/image-three.png'
+} as IProduct
+
 export * from '@testing-library/react'
 export { render }
